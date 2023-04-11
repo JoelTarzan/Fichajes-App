1ef91c561cec0e23ebd48f24d18f98cf70316d17
joel <joelth368@gmail.com>
2023-04-11 19:26:18 +0200

    TypeORM instalado, dotenv instalado, conexion a la db y creado el modulo 'empleados'

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,26 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { EmpleadosModule } from './empleados/empleados.module';
+import { TypeOrmModule } from "@nestjs/typeorm";
+import * as dotenv from 'dotenv';
+
+dotenv.config();
 
 @Module({
-  imports: [],
+  imports: [
+      TypeOrmModule.forRoot({
+          type: 'mariadb',
+          host: process.env.DB_HOST,
+          port: parseInt(process.env.DB_PORT),
+          username: process.env.DB_USER,
+          password: process.env.DB_PASSWORD,
+          database: 'fichajes_app',
+          entities: [__dirname + '**/*.entity{.ts, .js}'],
+          synchronize: true
+      }),
+      EmpleadosModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
